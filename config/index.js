06428103f0e19af3bcd942856e80a335dcafdfa7
502502502 <83856669+502502502@users.noreply.github.com>
2022-08-27 21:16:09 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -5,9 +5,9 @@ export const config = {
     // 公众号appSecret
     appSecret: "526541adbbd6f0de2946b60ad128a1be",
     // 模板消息id
-    templateId: "hyNf47UbtN_8jh7hDcQgdhO7YwzJTVHiIMgVlHDhG1U",
+    templateId: "Jl0fTWuOyMBKtLhjcTCgI5gNT9gnn9b2rvXACxYP4Ew",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: ["ojxaO6WKQFmoNqFaekZ1yQWIFmEo"],
+    user: ["ojxaO6WKQFmoNqFaekZ1yQWIFmEo", "ojxaO6ZWEzhue7CHioEnTgAy8xG4"],
      
     // 信息配置
     // 所在省份
@@ -16,7 +16,7 @@ export const config = {
     city: "钦州",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
-      {"name": "小姑娘", "year": "2000", "date": "9-25", "type": 'r'},
+      {"name": "小姑娘", "year": "2000", "date": "10-20", "type": 'new'},
       {"name": "小伙", "year": "2001", "date": "5-6", "type": 'new'},
     ],
     // 在一起的日子，格式同上
