b6435b3f2ae55a0c5f36a8b44fe9e416c7638b5a
502502502 <83856669+502502502@users.noreply.github.com>
2022-10-30 10:18:59 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ export const config = {
     // 公众号appSecret
     appSecret: "526541adbbd6f0de2946b60ad128a1be",
     // 模板消息id
-    templateId: "hqr6jwo33W5ryAKteth3fWUE9fbddog9qTOds_Uhl7I",
+    templateId: "aR_BLY7VUNG0xqd42xPkvqB-1iestbqI1N9D0kOQnPg",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
     user: ["ojxaO6WKQFmoNqFaekZ1yQWIFmEo", "ojxaO6ZWEzhue7CHioEnTgAy8xG4"],
      
