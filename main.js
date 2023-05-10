7bd1b529380f357180319b1a680055a50691dbe4
502502502 <83856669+502502502@users.noreply.github.com>
2023-05-10 22:00:32 +0800

    Update main.js

--- main.js
+++ main.js
@@ -43,9 +43,9 @@ const main = async () => {
     // 获取金山词霸每日一句
     const { content: noteCh, note: noteEn} = await getCIBA()
     // 获取在一起的日期差
-    const loveDay = dayjs().diff(dayjs(config.loveDate), 'day')
+    const loveDay = dayjs().ad(1,'day').diff(dayjs(config.loveDate), 'day')
     // 获取结婚的日期差
-    const marryDay = dayjs().diff(dayjs(config.marryDate), 'day')
+    const marryDay = dayjs().add(1,'day').diff(dayjs(config.marryDate), 'day')
     // 获取生日信息
     const birthdayMessage = getBirthdayMessage()
     // 获取考研信息
@@ -62,7 +62,7 @@ const main = async () => {
     // 集成所需信息
     const week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     const wxTemplateParams = [
-        { name: toLowerLine('date'), value: `${dayjs().format('YYYY-MM-DD')} ${week_list[dayjs().format('d')]}`, color: getColor() },
+        { name: toLowerLine('date'), value: `${dayjs().add(1,'day').format('YYYY-MM-DD')} ${week_list[dayjs().add(1,'day').format('d')]}`, color: getColor() },
         { name: toLowerLine('province'), value: province, color: getColor() },
         { name: toLowerLine('city'), value: city, color: getColor() },
         { name: toLowerLine('weather'), value: weather, color: getColor() },
